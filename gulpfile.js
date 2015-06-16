205a351234ee57a638a7d5d5f71fa2eeb69eece8
Steven Ng <steven.ng@braintapper.com>
2015-06-16 17:37:34 -0400

    Gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,5 @@
+// Note the new way of requesting CoffeeScript since 1.7.x
+// require('coffee-script/register');
+require('coffee-script');
+// This bootstraps your Gulp's main file
+require('./gulpfile.coffee');
